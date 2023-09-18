36983f9ee34921e7199a73199a125aaa0ec5c688
sethdroberts <sethknock@gmail.com>
2023-09-18 23:59:35 +0000

    Initial repository

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Seth Roberts", page_icon=":victory_hand:", layout="wide")
+
+#HEADER SECTION
+st.subheader("Hi, I am Seth :wave:")
+st.write("I am passionate about using literature evangelism to change the lives of young people around the world.")
+st.write("[Learn More >](https://www.miyouthrush.org)")
